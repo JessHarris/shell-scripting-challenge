d3488ffab727eeb874d31b6727a49b84252ba5c6
JessHarris <jess.harristn@gmail.com>
2019-03-26 13:55:42 +0000

    adding debugging exercise description

--- udemy-course/debugging/debugging_1.sh
+++ udemy-course/debugging/debugging_1.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
+#Write a shell script that exit on error and displays commands as they will execute, including all
+#expansions and substitutions. Use 3 ls commands in your script. Make the first one succeed,
+#the second one fail, and the third one succeed. If you are using the proper options, the third ls
+#command will not be executed.
+
 ls directoryThatExists/
 ls directoryThatDoesntExist/
 ls directoryThatExits/
