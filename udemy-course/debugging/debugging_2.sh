de5272a53057461af49e58b7ec6403bbef55709a
JessHarris <jess.harristn@gmail.com>
2019-03-26 13:56:20 +0000

    adding debugging exercise description

--- udemy-course/debugging/debugging_2.sh
+++ udemy-course/debugging/debugging_2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+#Modify the previous exercise so that script continues, even if an error occurs. This time all three
+#ls commands will execute.
+
 ls directoryThatExists/
 ls directoryThatDoesntExist/
 ls directoryThatExits/
