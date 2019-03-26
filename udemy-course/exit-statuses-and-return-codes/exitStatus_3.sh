8b828f2a88e2530a6de8995a19e6de6a4a7d6c55
JessHarris <jess.harristn@gmail.com>
2019-03-26 14:01:50 +0000

    adding exit status exercise description

--- udemy-course/exit-statuses-and-return-codes/exitStatus_3.sh
+++ udemy-course/exit-statuses-and-return-codes/exitStatus_3.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Write a script that executes the command "cat /etc/shadow". If the command returns a 0 exit
+#status report "Command succeeded" and exit with a 0 exit status. If the command returns a
+#nonzero exit status report "Command failed" and exit with a 1 exit status.
 
 cat /etc/shadow
-
 if [ "$?" -eq "0" ]
 	then
 		echo "Command Succeeded"
