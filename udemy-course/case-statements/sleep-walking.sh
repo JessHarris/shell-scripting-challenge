561db534467d1519984d99e623202e83918d5f5c
JessHarris <jess.harristn@gmail.com>
2019-03-26 13:46:47 +0000

    adding exercise description

--- udemy-course/case-statements/sleep-walking.sh
+++ udemy-course/case-statements/sleep-walking.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#Create a startup script for an application called sleepwalkingserver,
+#The script should be named sleepwalking and accept "start" and "stop" as arguments. If
+#anything other than "start" or "stop" is provided as an argument, display a usage statement:
+#"Usage sleepwalking start|stop" and terminate the script with an exit status of 1.
+
 #Script to start and stop sleep-walking-server
-case $1 in 
+case "$1" in 
 	start)
 		./sleep-walking-server.sh
 		;;
@@ -10,7 +15,6 @@ case $1 in
 	*)
 		echo "Usage sleepwalking start|stop"
 		exit 1
-		;;
 esac
 
 	
